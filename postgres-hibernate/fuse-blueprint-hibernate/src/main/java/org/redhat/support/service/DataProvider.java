f34551f223d16bc5444100f33086c2a8bf103407
Swapnil Kumbhar <skumbhar@skumbhar.pnq.csb>
2017-04-03 13:38:59 +0530

    postgresql

---
+++ postgres-hibernate/fuse-blueprint-hibernate/src/main/java/org/redhat/support/service/DataProvider.java
@@ -0,0 +1,16 @@
+package org.redhat.support.service;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.redhat.support.entity.Billing;
+
+public class DataProvider implements Processor{
+
+	public void process(Exchange ex) throws Exception {
+		// TODO Auto-generated method stub
+		Billing billing = new Billing();
+		billing.setBillingEventId(112L);
+		ex.getOut().setBody(billing);
+	}
+
+}
